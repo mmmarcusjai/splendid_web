37018899c935fde0e81db93a1f57149004e1402f
marcuschan <>
2018-07-20 18:17:58 +0800

    no message

--- themes/productions/assets/javascript/common.js
+++ themes/productions/assets/javascript/common.js
@@ -2,10 +2,13 @@ var facilities_top = document.querySelector('.facilities').offsetTop;
 var quality_top = document.querySelector('.quality-content').offsetTop;
 var vid = document.querySelector('.facilities video');
 
-window.onscroll = () => {
+window.onscroll = function() {
     const nav = document.querySelector('nav');
     var class_name = '';
     var position;
+    console.log('current ::::: ' + scrollY );
+    console.log('facilities_top ::::: ' + facilities_top );
+    console.log('quality_top ::::: ' + quality_top );
     if(this.scrollY <= 10) {
         class_name = '';
     } else if(this.scrollY >= quality_top){
@@ -14,7 +17,7 @@ window.onscroll = () => {
         class_name = 'scroll-home';
     }
 
-    if(this.scrollY >= facilities_top && this.scrollY < quality_top) {
+    if(this.scrollY > facilities_top && this.scrollY < quality_top) {
         class_name = 'hide';
         vid.play();
     } else {
