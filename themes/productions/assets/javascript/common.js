cd4198383f3a9c5014eab418a9fe01a864d99e9c
marcuschan <>
2018-07-23 18:28:08 +0800

    marcus - update

--- themes/productions/assets/javascript/common.js
+++ themes/productions/assets/javascript/common.js
@@ -2,25 +2,22 @@ var facilities_top = document.querySelector('.facilities').offsetTop;
 var quality_top = document.querySelector('.quality-content').offsetTop;
 var vid = document.querySelector('.facilities video');
 
-window.onscroll = function() {
+window.onscroll = () => {
     const nav = document.querySelector('nav');
     var class_name = '';
     var position;
-    console.log('current ::::: ' + scrollY );
-    console.log('facilities_top ::::: ' + facilities_top );
-    console.log('quality_top ::::: ' + quality_top );
+
     if(this.scrollY <= 10) {
         class_name = '';
+        vid.pause();
     } else if(this.scrollY >= quality_top){
         class_name = 'scroll-quality';
-    } else {
-        class_name = 'scroll-home';
-    }
-
-    if(this.scrollY > facilities_top && this.scrollY < quality_top) {
+        vid.pause();
+    } else if(this.scrollY > facilities_top && this.scrollY < quality_top) {
         class_name = 'hide';
         vid.play();
     } else {
+        class_name = 'scroll-home';
         vid.pause();
     }
 
